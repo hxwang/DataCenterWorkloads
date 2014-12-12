9afc01754c9084a8be7d5f023cdc5084f0fcf391
Huangxin <maggiewangsjtu@gmail.com>
2014-12-12 14:44:23 -0500

    add data

--- process-grid5k/process-grid5k.py
+++ process-grid5k/process-grid5k.py
@@ -1,7 +1,7 @@
 from pystats import *
 
 processTimeField = 4
-skipLines = 11
+skipLines = 12
 with open('../data/anon_jobs_gwf/testSample.log') as f:
     res = stats(stream = f, field=processTimeField, delimiter=' ', skip = skipLines, confidence=0.95)
     print res
