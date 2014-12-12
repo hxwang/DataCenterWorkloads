79b3e7e0b7a0405c5b565ca6ceada6cb64b9c44c
Huangxin <maggiewangsjtu@gmail.com>
2014-12-12 16:17:10 -0500

    update scriptes

--- process-grid5k/filterUnExecuteJobs.py
+++ process-grid5k/filterUnExecuteJobs.py
@@ -1,14 +1,18 @@
 from pystats import *
+import re
 
-#filter unstarted workloads
+patt = re.compile("[^\t]+")
+#filter unstarted workloads, which has value in column smaller than 0
 def filterUnExecuteJobs():
 	with open('../data/anon_jobs_gwf/grid5000_clean_trace.log') as f:
 		outfile = open('../data/anon_jobs_gwf/filtered_grid5000_clean_trace.log','w')
-		skip = 12
+		skip = 13
 		colIndex = 3
 		lines = f.readlines()[skip:]
 		for line in lines:
-			if (int)(line[colIndex]) > 0:
+			items = patt.findall(line)      
+			# print len(items)
+			if (int)(items[colIndex]) > 0:
 				outfile.write(line +'\n')
 
     
--- process-grid5k/process-grid5k.py
+++ process-grid5k/process-grid5k.py
@@ -2,6 +2,6 @@ from pystats import *
 
 processTimeField = 4
 skipLines = 11
-with open('../data/anon_jobs_gwf/grid5000_clean_trace.log') as f:
-    res = stats(stream = f, field=processTimeField, delimiter='\t', skip = skipLines, confidence=0.95)
+with open('../data/anon_jobs_gwf/filtered_grid5000_clean_trace.log') as f:
+    res = stats(stream = f, field=processTimeField, delimiter='\t', skip = skipLines, confidence=0.95, navalue = '-1')
     print res
