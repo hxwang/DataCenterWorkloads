9b5197a846d061cababb5bb18a2207337c131fbc
Huangxin <maggiewangsjtu@gmail.com>
2015-01-17 18:19:26 -0500

    add

--- process-intrepid/getColumn.py
+++ process-intrepid/getColumn.py
@@ -1,12 +1,16 @@
+import sys
+
 def getColumn(skip, column, inputfile, outputfile):
   with open(inputfile, 'r') as f:
-    lines_after_skip = f.readlines()[skip:]
+    lines_after_skip = f.readlines()[(int)(skip):]
 
-  with open(outputfile, 'w') as f:
+  print len(lines_after_skip)
+  with open(outputfile, 'w') as f2:
     for line in lines_after_skip:
       items = line.split(' ')
-      if len(items) > column:
-        print >> f, items[column]
+      if len(items) >= column:
+      	print items[column]
+        print >> f2, items[column]
 
 if __name__ == '__main__':
-  getColumn(skip, column, inputfile, outputfile)
+  getColumn( sys.argv[1], sys.argv[2], sys.argv[3],sys.argv[4])
